7975be576b4f8e42a22b52ac5edddef212fa5a60
Ubuntu <debasis.comp.engg@gmail.com>
2021-05-15 13:52:42 +0000

    2nd commmit for integration

---
+++ src/main/java/com/simpliproject/docker_jenkins_integration/MyApp.java
@@ -0,0 +1,12 @@
+package com.simpliproject.docker_jenkins_integration;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("2nd changes in code ");
+
+	}
+
+}
